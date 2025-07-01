81e6b38c19233aaef79ca72e76b38841b991c372
Shubham Kuttarmare <shubhamkuttarmare8329@gmail.com>
2025-07-01 09:22:36 +0530

    changes done on 30 June

---
+++ server/routes/showRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getNowPlayingMovies } from "../controllers/showController.js";
+
+
+const showRouter = express.Router();
+
+showRouter.get('/now-playing', getNowPlayingMovies)
+
+
+export default showRouter;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 //make this change in all impots
 
 
@@ -24,6 +25,8 @@ app.use(clerkMiddleware())
 app.get('/', (req, res)=> res.send('Server is Live!'))
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use('/api/show', showRouter)
 
 
 app.listen(port, ()=> console.log(`Server listening at http://localhost:${port}`));
