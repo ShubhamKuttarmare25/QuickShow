8823f5d72d00469d327b025330f097aeee10f14d
Shubham Kuttarmare <shubhamkuttarmare8329@gmail.com>
2025-11-17 23:26:58 +0530

    fix: add navigation to navigate to my-booking page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import {
   MenuIcon,
@@ -14,6 +14,8 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { user } = useUser();
   const { openSignIn } = useClerk();
+
+  const navigate = useNavigate()
 
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
