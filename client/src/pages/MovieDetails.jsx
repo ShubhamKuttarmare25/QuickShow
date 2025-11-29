3b59c482c4c4cb58e87e9d588337e9cb4dca1e3f
Shubham Kuttarmare <shubhamkuttarmare8329@gmail.com>
2025-11-30 00:40:13 +0530

    feat: add Loading component and integrate it into MovieDetails for improved loading state handling

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -6,6 +6,7 @@ import { Heart, PlayCircle, StarIcon } from 'lucide-react'
 import timeFormat from '../lib/timeFormat'
 import DateSelect from '../components/DateSelect'
 import MovieCard from '../components/MovieCard'
+import Loading from '../components/Loading'
 
 const MovieDetails = () => {
 
@@ -15,10 +16,13 @@ const MovieDetails = () => {
 
   const getShow = async()=>{
     const show = dummyShowsData.find(show => show._id === id)
-    setShow({
+    if(show){
+      setShow({
       movie: show,
       dateTime: dummyDateTimeData 
     })
+    }
+    
   }
 
   useEffect(()=>{
@@ -88,7 +92,7 @@ const MovieDetails = () => {
         </div>
 
     </div>
-  ) : <div>Loading....</div>
+  ) : <Loading />
 }
 
 export default MovieDetails
